9f68642e26bc1f3e30e4046e9a90cf7e85329cbb
Shaw <michele_aliffi@yahoo.it>
2021-12-01 16:13:22 +0100

    Fine progetto

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -14,18 +14,13 @@ public class Main {
 		boolean continua = false;
 		
 		while(continua == false) {
-			// chiedere all`utente l`inserimento del regalo
 			System.out.print("Inserisci il regalo che vuoi: ");
 			gifts.add(scanner.nextLine());
-			// stampare tutti i regali inseriti fino ad ora
-			System.out.println(gifts.size());
-			// chiedere se vuole continuare con l`inserimento
+			System.out.println("La lista è composta da: " + gifts.size() + " elementi");
 			System.out.print("Vuoi inserire altro? (s/n)");
 			String scelta = scanner.nextLine();
-			// se la risposta è si il ciclo ricomincia
 			if(scelta.equalsIgnoreCase("n")) {
 				continua = true;
-			// se la risposta è no il ciclo finisce
 			} else if(scelta.equalsIgnoreCase("s")) {
 				
 			} else {
@@ -39,25 +34,6 @@ public class Main {
 			System.out.println(gifts.get(i));
 		}
 		
-//		System.out.print("Inserisci il regalo che vuoi: ");
-//		gifts.add(scanner.nextLine());
-//		
-//		
-//		System.out.print("Vuoi inserire altro? (s/n)");
-//		String scelta = scanner.nextLine();
-//		
-//		
-//		for(int i = 0; i < gifts.size(); i++) {
-//			System.out.println(gifts.get(i));
-//		}
-//		
-//		
-//		if(scelta.equalsIgnoreCase("s")) {
-//			gifts.add(scanner.nextLine());
-//		} else {
-//			System.out.println("La tua lista è composta da: " + gifts.get(i) + "elementi");
-//			System.out.println(gifts);
-//		}
 		scanner.close();	
 	}
 
